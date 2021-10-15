76fa902202707d43b5fc0a2ac0608f214bcc6987
Jhon Calderon <ingdesarrollo1@grupodyp.com>
2021-10-15 17:25:03 -0500

    Query

--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -15,10 +15,30 @@ const queryResolvers: IResolvers = {
       return `Hola ${args.name}`;
     },
     peopleNumber: () => 103,
-    bookList: (): Array<IBook> => data.books,
+    bookList: (): {
+      status: boolean;
+      message: string;
+      list: Array<IBook>;
+    } => ({ status: true, message: "Lista de libros", list: data.books }),
     peopleList: (): Array<IPeople> => data.people,
-    book: (_: void, args: { id: string }): IBook => data.books.filter((b) => b.id === args.id)[0],
-    person: (_: void, args: { id: string }): IPeople => data.people.filter((p) => p.id === args.id)[0]  
+    book: (
+      _: void,
+      args: { id: string }
+    ): {
+      status: boolean;
+      message: string;
+      item: IBook;
+    } => {
+      const bookFind = data.books.filter((b) => b.id === args.id)[0];
+      return {
+        status: bookFind === undefined ? false: true,
+        message: bookFind === undefined ? `Libro con el id: ${args.id} encontrado`: `No se encontro el libro con el id: ${args.id}`,
+        item: bookFind,
+      
+      } 
+    },
+    person: (_: void, args: { id: string }): IPeople =>
+      data.people.filter((p) => p.id === args.id)[0],
   },
 };
 
